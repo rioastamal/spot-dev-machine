e283e7e58de047b86b3bc9ea41f3455cc5762968
Rio Astamal <rio@rioastamal.net>
2022-02-05 02:49:39 +0000

    roles.tf: Removing unnecessary permissions

--- roles.tf
+++ roles.tf
@@ -21,17 +21,12 @@ resource "aws_iam_role" "dev_machine_role" {
     ]
   })
 
-  managed_policy_arns = [ 
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
-    "arn:aws:iam::aws:policy/CloudWatchFullAccess"
-  ]
-
   inline_policy {
     name = "dev_s3_bucket"
-		policy = jsonencode({
-			Version = "2012-10-17"
-			Statement = [
-				{
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
           Effect = "Allow",
           Action = [
             "s3:ListBucket",
@@ -39,128 +34,47 @@ resource "aws_iam_role" "dev_machine_role" {
             "s3:DeleteBucket"
           ]
           Resource = "arn:aws:s3:::${var.dev_bucket_name}"
-				},
-				{
+        },
+        {
           Effect = "Allow",
           Action = [
             "s3:*",
             "s3-object-lambda:*"
           ]
           Resource = "arn:aws:s3:::${var.dev_bucket_name}/*"
-				}
-			]
-		})
+        }
+      ]
+    })
   }
-  # s3
+  # S3
 
   inline_policy {
     name = "dev_ssm_access"
-		policy = jsonencode({
-			Version = "2012-10-17"
-			Statement = [
-				{
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
           Effect = "Allow",
           Action = [
-          	"ds:CreateComputer",
-            "ds:DescribeDirectories",
-            "logs:*",
-            "ssm:*",
-            "ec2messages:*"
+            "ssm:PutParameter",
+            "ssm:DeleteParameter",
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter",
+            "ssm:DeleteParameters"
           ]
-          Resource = "*"
+          Resource = "arn:aws:ssm:::parameter/dev_*"
         },
-				{
+        {
           Effect = "Allow",
           Action = [
-            "iam:CreateServiceLinkedRole",
-            "iam:DeleteServiceLinkedRole",
-            "iam:GetServiceLinkedRoleDeletionStatus"
+            "ssm:DescribeParameters"
           ]
-          Resource = "arn:aws:iam::*:role/aws-service-role/ssm.amazonaws.com/AWSServiceRoleForAmazonSSM*"
-          Condition = {
-            "StringLike" = {
-              "iam:AWSServiceName" = "ssm.amazonaws.com"
-            }
-          }
-				},
-				{
-          Effect = "Allow",
-          Action = [
-                "ssmmessages:CreateControlChannel",
-                "ssmmessages:CreateDataChannel",
-                "ssmmessages:OpenControlChannel",
-                "ssmmessages:OpenDataChannel"          
-					]
           Resource = "*"
-				},
-			]
-		})
+      ]
+    })
   }
   # SSM
-
-  inline_policy {
-    name = "dev_efs"
-		policy = jsonencode({
-			Version = "2012-10-17"
-			Statement = [
-				{
-          Effect = "Allow",
-          Action = [
-                "elasticfilesystem:ClientMount",
-                "elasticfilesystem:ClientWrite",
-                "elasticfilesystem:ClientRootAccess",
-                "elasticfilesystem:CreateFileSystem",
-                "elasticfilesystem:CreateMountTarget",
-                "elasticfilesystem:CreateTags",
-                "elasticfilesystem:CreateAccessPoint",
-                "elasticfilesystem:CreateReplicationConfiguration",
-                "elasticfilesystem:DeleteFileSystem",
-                "elasticfilesystem:DeleteMountTarget",
-                "elasticfilesystem:DeleteTags",
-                "elasticfilesystem:DeleteAccessPoint",
-                "elasticfilesystem:DeleteFileSystemPolicy",
-                "elasticfilesystem:DeleteReplicationConfiguration",
-                "elasticfilesystem:DescribeAccountPreferences",
-                "elasticfilesystem:DescribeBackupPolicy",
-                "elasticfilesystem:DescribeFileSystems",
-                "elasticfilesystem:DescribeFileSystemPolicy",
-                "elasticfilesystem:DescribeLifecycleConfiguration",
-                "elasticfilesystem:DescribeMountTargets",
-                "elasticfilesystem:DescribeMountTargetSecurityGroups",
-                "elasticfilesystem:DescribeTags",
-                "elasticfilesystem:DescribeAccessPoints",
-                "elasticfilesystem:DescribeReplicationConfigurations",
-                "elasticfilesystem:ModifyMountTargetSecurityGroups",
-                "elasticfilesystem:PutAccountPreferences",
-                "elasticfilesystem:PutBackupPolicy",
-                "elasticfilesystem:PutLifecycleConfiguration",
-                "elasticfilesystem:PutFileSystemPolicy",
-                "elasticfilesystem:UpdateFileSystem",
-                "elasticfilesystem:TagResource",
-                "elasticfilesystem:UntagResource",
-                "elasticfilesystem:ListTagsForResource",
-                "elasticfilesystem:Backup",
-                "elasticfilesystem:Restore",
-                "kms:DescribeKey",
-                "kms:ListAliases"          
-					]
-          Resource = "*"
-				},
-				{
-          Effect = "Allow",
-          Action = [
-            "iam:CreateServiceLinkedRole"
-          ]
-          Resource = "*"
-          Condition = {
-            StringEquals = {
-              "iam:AWSServiceName" = [ "elasticfilesystem.amazonaws.com" ]
-            }
-          }
-				}
-			]
-		})
-  }
-  # EFS
 }
 
