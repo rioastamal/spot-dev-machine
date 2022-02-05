7bb85f81ab765d5fc54d7fce2df9a712b5ffbb6e
Rio Astamal <rio@rioastamal.net>
2022-02-05 11:59:16 +0000

    Vim retab

--- main.tf
+++ main.tf
@@ -29,14 +29,14 @@ resource "aws_security_group" "dev_machine_firewall" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-		cidr_blocks			 = [ "0.0.0.0/0" ]
+    cidr_blocks      = [ "0.0.0.0/0" ]
   }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-		cidr_blocks			 = [ "0.0.0.0/0" ]
+    cidr_blocks      = [ "0.0.0.0/0" ]
   }
 }
 
@@ -49,14 +49,14 @@ resource "aws_security_group" "dev_efs_firewall" {
     from_port        = 2049
     to_port          = 2049
     protocol         = "tcp"
-		security_groups = [ aws_security_group.dev_machine_firewall.id ]
+    security_groups = [ aws_security_group.dev_machine_firewall.id ]
   }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-		cidr_blocks			 = [ "0.0.0.0/0" ]
+    cidr_blocks      = [ "0.0.0.0/0" ]
   }
 }
 
@@ -72,8 +72,8 @@ resource "aws_efs_file_system" "dev_efs" {
     transition_to_primary_storage_class = "AFTER_1_ACCESS"
   }
 
-	lifecycle {
-		prevent_destroy = true
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "dev_machine_info" {
   value = {
-		vpc_id = data.aws_vpc.default.id
-  	public_ipv4 = aws_eip.dev_machine_ip.public_ip
+    vpc_id = data.aws_vpc.default.id
+    public_ipv4 = aws_eip.dev_machine_ip.public_ip
     efs_id = aws_efs_file_system.dev_efs.id
     access_point_data = aws_efs_access_point.dev_efs_main_ap.id
     access_point_docker = aws_efs_access_point.dev_efs_docker_ap.id
-	}
+  }
 }
 
 output "efs_mount" {
