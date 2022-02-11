ecd149a878342bc840d4cc4c094724254820da37
Rio Astamal <me@rioastamal.net>
2022-02-11 15:56:21 +0700

    install-terraform script: Download binary release directly instead via package manager

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "Installing Terraform..."
-yum install -q -y yum-utils && \
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo && \
-yum install -q -y terraform
+TERRAFORM_VERSION="1.1.5"
+TERRAFORM_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+curl -L -s -o /tmp/terraform-${TERRAFORM_VERSION}.zip "$TERRAFORM_URL"
+sudo unzip /tmp/terraform-${TERRAFORM_VERSION}.zip -d /usr/local/bin/
