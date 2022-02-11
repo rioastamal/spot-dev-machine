1d9fb6d3b1da4c06cddcba41ec175d5e3eaf9fae
Rio Astamal <me@rioastamal.net>
2022-02-11 11:25:13 +0700

    user-data script: Fix `tee` command when writing ssh public key

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -46,7 +46,10 @@ mount -t efs -o az=$EFS_MOUNT_AZ,tls,accesspoint=$ACCESS_POINT_DOCKER $EFS_ID:/ 
 echo "Inserting public SSH key into EFS home directory..."
 sudo -u ec2-user mkdir $HOME_DIR/.ssh
 sudo -u ec2-user chmod 0700 $HOME_DIR/.ssh
-grep -q -F "${SSH_PUBLIC_KEY}" $HOME_DIR/.ssh/authorized_keys || sudo -u ec2-user tee -a "${SSH_PUBLIC_KEY}" $HOME_DIR/.ssh/authorized_keys
+sudo -u ec2-user touch $HOME_DIR/.ssh/authorized_keys
+grep -q -F "${SSH_PUBLIC_KEY}" $HOME_DIR/.ssh/authorized_keys || {
+  echo "${SSH_PUBLIC_KEY}" | sudo -u ec2-user tee -a "${SSH_PUBLIC_KEY}" $HOME_DIR/.ssh/authorized_keys
+}
 
 GIT_REPO=rioastamal/spot-dev-machine
 RAW_GIT_URL=https://raw.githubusercontent.com/${GIT_REPO}/master/scripts
